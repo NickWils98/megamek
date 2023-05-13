0894112eec52e0780b00e6647002dcade1be3bb9
Senne <senne@lemon.be>
2023-05-13 14:12:14 +0200

    Added base for PlayerTest

---
+++ megamek/unittests/megamek/common/PlayerTest.java
@@ -0,0 +1,27 @@
+package megamek.common;
+
+import junit.framework.TestCase;
+import megamek.common.*;
+import megamek.server.Server;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+@RunWith(JUnit4.class)
+public class PlayerTest {
+
+    @Test
+    public void getPlayerColorTest() throws IOException  {
+        IPlayer player = new Player(0, "TestPlayer");
+        Server server = new Server("TestServer", 1120);
+        IGame mockedGame = Mockito.mock(IGame.class);
+
+        server.setGame(mockedGame);
+
+//        TestCase.assertEquals("<B><font color='8080b0'> TestPlayer </font></B>", server.getColorForPlayer(player));
+    }
+}
